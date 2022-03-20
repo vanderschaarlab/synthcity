2d0c147ce8faad66a0fb660388169c57abf2c55a
Bogdan Cebere <bogdan.cebere@gmail.com>
2022-03-20 12:44:17 +0000

    add anonymization techniques

--- src/synthcity/metrics/anonymity.py
+++ src/synthcity/metrics/anonymity.py
@@ -1,21 +1,190 @@
 # stdlib
+from collections import Counter
 from typing import Dict, List, Optional, Tuple
 
 # third party
+import numpy as np
 import pandas as pd
 from pydantic import validate_arguments
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import LabelEncoder
 
 
-class kAnonimity:
+class DatasetAnonymization:
+    """Dataset Anonymization helper based on the k-Anonymization, l-Diversity and t-Closeness methods.
+
+    k-Anonymity states that every individual in one dataset partition is indistinguishable from at least k - 1 other individuals.
+    l-Diversity uses a stronger privacy definition and claims that every generalized block has to contain at least l different sensitive values.
+    An equivalence class is said to have t-closeness if the distance between the distribution of a sensitive attribute in this class and the distribution of the attribute in the whole table is no more than a threshold t. A table is said to have t-closeness if all equivalence classes have t-closeness.
+    For that, we measure the Kolmogorov-Smirnov distance between the empirical probability distribution of the sensitive attribute over the entire dataset vs. the distribution over the partition.
+
+
+    """
+
+    @validate_arguments
     def __init__(
         self,
-        k: int = 10,
+        k_threshold: int = 10,
+        l_diversity: int = 2,
+        t_threshold: float = 0.2,
         categorical_limit: int = 5,
         max_partitions: Optional[int] = None,
     ) -> None:
+        if k_threshold < 1:
+            raise ValueError(
+                f"Invalid value for k_threshold = {k_threshold}. Must be >= 1"
+            )
+        if l_diversity < 1:
+            raise ValueError(
+                f"Invalid value for l_threshold = {l_diversity}. Must be >= 1"
+            )
+        if t_threshold > 1:
+            raise ValueError(
+                f"Invalid value for t_threshold = {t_threshold}. Must be < 1"
+            )
+        self.k_threshold = k_threshold
+        self.l_diversity = l_diversity
+        self.t_threshold = t_threshold
+
         self.categorical_limit = categorical_limit
-        self.k = k
         self.max_partitions = max_partitions
+
+    @validate_arguments(config=dict(arbitrary_types_allowed=True))
+    def is_anonymous(self, X: pd.DataFrame, sensitive_columns: List[str] = []) -> bool:
+        """True if the dataset is valid according to the k-anonymity criteria, False otherwise."""
+        if len(sensitive_columns) == 0:
+            sensitive_columns = X.columns
+
+        max_clusters = int(len(X) / (2 * self.k_threshold))
+        for n_clusters in [2, 5, 10, max_clusters]:
+            cluster = KMeans(
+                n_clusters=n_clusters, init="k-means++", random_state=0
+            ).fit(X[sensitive_columns])
+            counts: dict = Counter(cluster.labels_)
+            min_presence = np.min(list(counts.values()))
+
+            if min_presence < self.k_threshold:
+                return False
+
+        return True
+
+    def _setup(self, X: pd.DataFrame) -> Tuple[pd.DataFrame, Dict]:
+        encoders = {}
+
+        for col in X.columns:
+            if X[col].dtype != "object":
+                continue
+
+            encoder = LabelEncoder()
+            X[col] = encoder.fit_transform(X[col])
+            encoders[col] = encoder
+
+        return X, encoders
+
+    def _tear_down(self, X: pd.DataFrame, encoders: Dict) -> pd.DataFrame:
+        for col in encoders:
+            X[col] = encoders[col].inverse_transform(X[col])
+
+        return X
+
+    @validate_arguments(config=dict(arbitrary_types_allowed=True))
+    def anonymize(
+        self,
+        X: pd.DataFrame,
+    ) -> pd.DataFrame:
+        for column in X.columns:
+            X = self.anonymize_column(X, column)
+
+        return X
+
+    @validate_arguments(config=dict(arbitrary_types_allowed=True))
+    def anonymize_columns(
+        self,
+        X: pd.DataFrame,
+        sensitive_columns: List[str],
+    ) -> pd.DataFrame:
+        for column in sensitive_columns:
+            X = self.anonymize_column(X, column)
+
+        return X
+
+    @validate_arguments(config=dict(arbitrary_types_allowed=True))
+    def anonymize_column(
+        self,
+        X: pd.DataFrame,
+        sensitive_column: str,
+    ) -> pd.DataFrame:
+        X, encoders = self._setup(X)
+        features = self._get_features(X, [sensitive_column])
+
+        partitions = self._partition_dataset(X, features, sensitive_column)
+
+        def agg_categorical_column(series: pd.Series) -> list:
+            # TODO
+            return [series.mean()]
+
+        def agg_numerical_column(series: pd.Series) -> list:
+            return [series.mean()]
+
+        categoricals = self._get_categoricals(X)
+
+        rows = []
+        for i, partition in enumerate(partitions):
+            if self.max_partitions is not None and i > self.max_partitions:
+                break
+
+            X_part = X.loc[partition]
+
+            X_part_cat = X_part[[c for c in X_part.columns if c in categoricals]]
+            X_part_num = X_part[[c for c in X_part.columns if c not in categoricals]]
+
+            num_agg = X_part_num.agg(agg_numerical_column)
+            cat_agg = X_part_cat.agg(agg_categorical_column)
+
+            aggregations = []
+
+            if len(num_agg) > 0:
+                aggregations.append(num_agg)
+            if len(cat_agg) > 0:
+                aggregations.append(cat_agg)
+
+            grouped_columns = pd.concat(
+                aggregations,
+                axis=1,
+            )
+
+            assert set(grouped_columns.columns) == set(X_part.columns)
+            grouped_columns = grouped_columns[X_part.columns]
+
+            sensitive_counts = X_part.groupby(sensitive_column).agg(
+                {sensitive_column: "count"}
+            )
+            values = grouped_columns.iloc[0].to_dict()
+
+            for sensitive_value, count in sensitive_counts[sensitive_column].items():
+                if count == 0:
+                    continue
+                values.update(
+                    {
+                        sensitive_column: sensitive_value,
+                    }
+                )
+                for cnt in range(count):
+                    rows.append(values.copy())
+
+        result = pd.DataFrame(rows)
+
+        return self._tear_down(result, encoders)
+
+    @validate_arguments(config=dict(arbitrary_types_allowed=True))
+    def _get_features(self, X: pd.DataFrame, sensitive_columns: List[str] = []) -> List:
+        """Return the non-sensitive features from dataset X"""
+        features = list(X.columns)
+        for col in sensitive_columns:
+            if col in features:
+                features.remove(col)
+
+        return features
 
     @validate_arguments(config=dict(arbitrary_types_allowed=True))
     def _get_spans(
@@ -52,6 +221,7 @@ class kAnonimity:
             dfr = Xpart.index[Xpart >= median]
             return (dfl, dfr)
 
+    @validate_arguments(config=dict(arbitrary_types_allowed=True))
     def _get_categoricals(self, X: pd.DataFrame) -> List:
         categoricals = []
         for column in X.columns:
@@ -65,6 +235,7 @@ class kAnonimity:
         self,
         X: pd.DataFrame,
         feature_columns: List,
+        sensitive_column: str,
     ) -> List:
         """Learn a list of valid partitions that covers the entire dataframe."""
         finished_partitions = []
@@ -72,14 +243,17 @@ class kAnonimity:
 
         scale = self._get_spans(X, X.index)
         categoricals = self._get_categoricals(X)
+        freqs = self._get_frequencies(X, sensitive_column)
 
         while partitions:
             partition = partitions.pop(0)
             spans = self._get_spans(X[feature_columns], partition, scale)
             for column, span in sorted(spans.items(), key=lambda x: -x[1]):
                 lp, rp = self._split(X, partition, column, categoricals)
-                if not self._is_index_k_anonymous(lp) or not self._is_index_k_anonymous(
-                    rp
+                if not self._is_partition_anonymous(
+                    X, lp, sensitive_column, freqs, categoricals
+                ) or not self._is_partition_anonymous(
+                    X, rp, sensitive_column, freqs, categoricals
                 ):
                     continue
                 partitions.extend((lp, rp))
@@ -89,74 +263,66 @@ class kAnonimity:
         return finished_partitions
 
     @validate_arguments(config=dict(arbitrary_types_allowed=True))
-    def _is_index_k_anonymous(self, partition: pd.Index) -> bool:
-        """True if the partition is valid according to our k-anonymity criteria, False otherwise."""
-        return len(partition) >= self.k
+    def _get_frequencies(self, X: pd.DataFrame, sensitive_column: str) -> Dict:
+        freq = {}
+        total_count = float(len(X))
+        group_counts = X.groupby(sensitive_column)[sensitive_column].agg("count")
+        for value, count in group_counts.to_dict().items():
+            p = count / total_count
+            freq[value] = p
+
+        return freq
 
     @validate_arguments(config=dict(arbitrary_types_allowed=True))
-    def anonymize(
+    def _is_k_anonymous(self, X: pd.DataFrame) -> bool:
+        """True if the dataset is valid according to the k-Anonymity criteria, False otherwise."""
+        return len(X) >= self.k_threshold
+
+    @validate_arguments(config=dict(arbitrary_types_allowed=True))
+    def _is_l_diverse(self, X: pd.DataFrame, sensitive_column: str) -> bool:
+        """True if the dataset is valid according to the l-Diversity criteria, False otherwise."""
+        return len(X[sensitive_column].unique()) >= self.l_diversity
+
+    @validate_arguments(config=dict(arbitrary_types_allowed=True))
+    def _t_closeness(self, X: pd.DataFrame, column: str, freqs: Dict) -> float:
+        cnt = float(len(X))
+        d_max = -1.0
+        group_counts = X.groupby(column)[column].agg("count")
+        for value, count in group_counts.to_dict().items():
+            p = count / (cnt + 1e-8)
+            d = abs(p - freqs[value])
+            if d > d_max:
+                d_max = d
+        return d_max
+
+    @validate_arguments(config=dict(arbitrary_types_allowed=True))
+    def _is_t_close(
         self,
         X: pd.DataFrame,
         sensitive_column: str,
-    ) -> pd.DataFrame:
-        features = list(X.columns)
-        features.remove(sensitive_column)
+        frequencies: Dict,
+        categorical_cols: List,
+    ) -> bool:
+        """True if the dataset is valid according to the t-closeness criteria, False otherwise."""
+        if sensitive_column not in categorical_cols:
+            return True
 
-        partitions = self._partition_dataset(X, features)
-
-        def agg_categorical_column(series: pd.Series) -> list:
-            return [",".join([str(v) for v in set(series)])]
-
-        def agg_numerical_column(series: pd.Series) -> list:
-            return [series.mean()]
-
-        categoricals = self._get_categoricals(X)
-
-        rows = []
-        for i, partition in enumerate(partitions):
-            if self.max_partitions is not None and i > self.max_partitions:
-                break
-
-            X_part = X.loc[partition]
-
-            X_part_cat = X_part[[c for c in X_part.columns if c in categoricals]]
-            X_part_num = X_part[[c for c in X.columns if c not in categoricals]]
-
-            grouped_columns = pd.concat(
-                [
-                    X_part_num.agg(agg_numerical_column),
-                    X_part_cat.agg(agg_categorical_column),
-                ],
-                axis=1,
-            )
-
-            sensitive_counts = X_part.groupby(sensitive_column).agg(
-                {sensitive_column: "count"}
-            )
-            values = grouped_columns.iloc[0].to_dict()
-            for sensitive_value, count in sensitive_counts[sensitive_column].items():
-                if count == 0:
-                    continue
-                values.update(
-                    {
-                        sensitive_column: sensitive_value,
-                    }
-                )
-                for cnt in range(count):
-                    rows.append(values.copy())
-
-        result = pd.DataFrame(rows)
-        if not self.is_k_anonymous(result, sensitive_column):
-            raise RuntimeError("Dataset anonymize failed")
-
-        return result
+        return self._t_closeness(X, sensitive_column, frequencies) <= self.t_threshold
 
     @validate_arguments(config=dict(arbitrary_types_allowed=True))
-    def is_k_anonymous(self, X: pd.DataFrame, sensitive_column: str) -> bool:
-        """True if the dataset is valid according to our k-anonymity criteria, False otherwise."""
-
-        features = list(X.columns)
-        features.remove(sensitive_column)
-        X_groupby = X.groupby(features).size().reset_index(name="count")
-
-        return bool(X_groupby["count"].min() >= self.k)
+    def _is_partition_anonymous(
+        self,
+        X: pd.DataFrame,
+        partition: pd.Index,
+        sensitive_column: str,
+        frequencies: Dict = {},
+        categorical_columns: List = [],
+    ) -> bool:
+        Xpartition = X.loc[partition]
+        return (
+            self._is_k_anonymous(Xpartition)
+            and self._is_l_diverse(Xpartition, sensitive_column)
+            and self._is_t_close(
+                Xpartition, sensitive_column, frequencies, categorical_columns
+            )
+        )
--- tests/metrics/test_anonymity.py
+++ tests/metrics/test_anonymity.py
@@ -1,20 +1,26 @@
 # third party
 import pytest
-from sklearn.datasets import load_breast_cancer
+from sklearn.datasets import load_breast_cancer, load_diabetes
 
 # synthcity absolute
-from synthcity.metrics.anonymity import kAnonimity
+from synthcity.metrics.anonymity import DatasetAnonymization
 
 
 def test_k_anonymity_sanity() -> None:
-    evaluator = kAnonimity()
+    evaluator = DatasetAnonymization(
+        k_threshold=44,
+        categorical_limit=11,
+        max_partitions=22,
+    )
 
-    assert evaluator.categorical_limit == 5
+    assert evaluator.categorical_limit == 11
+    assert evaluator.k_threshold == 44
+    assert evaluator.max_partitions == 22
 
 
 def test_k_anonymity_get_spans() -> None:
     X, y = load_breast_cancer(return_X_y=True, as_frame=True)
-    evaluator = kAnonimity()
+    evaluator = DatasetAnonymization()
 
     spans = evaluator._get_spans(X, X.index)
 
@@ -23,7 +29,7 @@ def test_k_anonymity_get_spans() -> None:
 
 def test_k_anonymity_split() -> None:
     X, y = load_breast_cancer(return_X_y=True, as_frame=True)
-    evaluator = kAnonimity()
+    evaluator = DatasetAnonymization()
 
     categoricals = evaluator._get_categoricals(X)
     for col in X.columns:
@@ -33,39 +39,355 @@ def test_k_anonymity_split() -> None:
         assert len(rhs) > 0
         assert len(lhs) + len(rhs) == len(X)
 
-        assert evaluator._is_index_k_anonymous(lhs) is True
-        assert evaluator._is_index_k_anonymous(rhs) is True
+        assert evaluator._is_partition_anonymous(X, lhs, X.columns[0]) is True
+        assert evaluator._is_partition_anonymous(X, rhs, X.columns[0]) is True
 
 
 def test_k_anonymity_partition() -> None:
     X, y = load_breast_cancer(return_X_y=True, as_frame=True)
-    evaluator = kAnonimity(k=100)
-    partitions = evaluator._partition_dataset(X, list(X.columns))
+    evaluator = DatasetAnonymization(k_threshold=100)
+    partitions = evaluator._partition_dataset(X, list(X.columns), X.columns[0])
     less_parts = len(partitions)
 
-    evaluator = kAnonimity(k=5)
-    partitions = evaluator._partition_dataset(X, list(X.columns))
+    evaluator = DatasetAnonymization(k_threshold=5)
+    partitions = evaluator._partition_dataset(X, list(X.columns), X.columns[0])
     more_parts = len(partitions)
 
+    assert less_parts > 0
+    assert more_parts > 0
     assert less_parts < more_parts
 
 
 def test_k_anonymity_validation() -> None:
     X, y = load_breast_cancer(return_X_y=True, as_frame=True)
 
-    evaluator = kAnonimity(k=1)
-    assert evaluator.is_k_anonymous(X) is True
+    evaluator = DatasetAnonymization(k_threshold=1)
+    assert evaluator.is_anonymous(X) is True
 
-    evaluator = kAnonimity(k=3)
-    assert evaluator.is_k_anonymous(X) is False
+    evaluator = DatasetAnonymization(k_threshold=3)
+    assert evaluator.is_anonymous(X) is False
+
+
+def test_k_anonymity_get_categoricals() -> None:
+    X, y = load_diabetes(return_X_y=True, as_frame=True)
+    X["target"] = y
+
+    evaluator = DatasetAnonymization()
+
+    assert evaluator._get_categoricals(X) == ["sex"]
+
+
+def test_k_anonymity_get_freqs() -> None:
+    X, y = load_diabetes(return_X_y=True, as_frame=True)
+    X["target"] = y
+
+    evaluator = DatasetAnonymization()
+
+    col = "sex"
+    freqs = evaluator._get_frequencies(X, col)
+    for val in X[col].unique():
+        assert val in freqs
+
+
+def test_k_anonymity_test_partition() -> None:
+    X, y = load_diabetes(return_X_y=True, as_frame=True)
+    X["target"] = y
+
+    evaluator = DatasetAnonymization()
+
+    assert evaluator._is_k_anonymous(X) is True
+    assert evaluator._is_k_anonymous(X[X["sex"] == 1234]) is False
 
 
 @pytest.mark.parametrize("k", [2, 5, 15, 30])
-def test_k_anonymity_anonymize(k: int) -> None:
-    X, y = load_breast_cancer(return_X_y=True, as_frame=True)
+def test_k_anonymity_anonymize_column(k: int) -> None:
+    X, y = load_diabetes(return_X_y=True, as_frame=True)
     X["target"] = y
 
-    evaluator = kAnonimity(k=k)
-    anon_df = evaluator.anonymize(X, sensitive_column="target")
+    sensitive_features = ["target"]
 
-    assert evaluator.is_k_anonymous(anon_df, "target") is True
+    evaluator = DatasetAnonymization(k_threshold=k)
+    assert evaluator.is_anonymous(X, sensitive_features) is False
+
+    anon_df = evaluator.anonymize_column(X, sensitive_column=sensitive_features[0])
+
+    assert evaluator.is_anonymous(anon_df, sensitive_features) is True
+    assert len(anon_df.drop_duplicates()) > 1
+
+
+@pytest.mark.parametrize("k", [2, 5, 15, 30])
+def test_k_anonymity_anonymize_columns(k: int) -> None:
+    X, y = load_diabetes(return_X_y=True, as_frame=True)
+    X["target"] = y
+
+    sensitive_features = list(X.columns)[:2]
+
+    evaluator = DatasetAnonymization(k_threshold=k)
+    assert evaluator.is_anonymous(X, sensitive_features) is False
+
+    anon_df = evaluator.anonymize_columns(X, sensitive_columns=sensitive_features)
+
+    assert evaluator.is_anonymous(anon_df, sensitive_features) is True
+    assert len(anon_df.drop_duplicates()) > 1
+
+
+@pytest.mark.parametrize("k", [2, 5, 15, 30])
+def test_k_anonymity_anonymize_full(k: int) -> None:
+    X, y = load_diabetes(return_X_y=True, as_frame=True)
+    X["target"] = y
+
+    evaluator = DatasetAnonymization(k_threshold=k)
+    assert evaluator.is_anonymous(X) is False
+
+    anon_df = evaluator.anonymize(X)
+
+    assert evaluator.is_anonymous(anon_df) is True
+    assert len(anon_df.drop_duplicates()) > 1
+
+
+def test_l_diversity_sanity() -> None:
+    evaluator = DatasetAnonymization(
+        k_threshold=44,
+        l_diversity=33,
+        categorical_limit=11,
+        max_partitions=22,
+    )
+
+    assert evaluator.categorical_limit == 11
+    assert evaluator.k_threshold == 44
+    assert evaluator.l_diversity == 33
+    assert evaluator.max_partitions == 22
+
+
+def test_l_diversity_get_spans() -> None:
+    X, y = load_breast_cancer(return_X_y=True, as_frame=True)
+    evaluator = DatasetAnonymization(l_diversity=20)
+
+    spans = evaluator._get_spans(X, X.index)
+
+    assert set(spans.keys()) == set(X.columns)
+
+
+def test_l_diversity_split() -> None:
+    X, y = load_breast_cancer(return_X_y=True, as_frame=True)
+    evaluator = DatasetAnonymization()
+
+    categoricals = evaluator._get_categoricals(X)
+    for col in X.columns:
+        lhs, rhs = evaluator._split(X, X.index, col, categoricals)
+
+        assert len(lhs) > 0
+        assert len(rhs) > 0
+        assert len(lhs) + len(rhs) == len(X)
+
+        assert evaluator._is_partition_anonymous(X, lhs, X.columns[0]) is True
+        assert evaluator._is_partition_anonymous(X, rhs, X.columns[0]) is True
+
+
+def test_l_diversity_partition() -> None:
+    X, y = load_breast_cancer(return_X_y=True, as_frame=True)
+    evaluator = DatasetAnonymization(l_diversity=20)
+    partitions = evaluator._partition_dataset(X, list(X.columns), X.columns[0])
+    less_parts = len(partitions)
+
+    evaluator = DatasetAnonymization(l_diversity=1)
+    partitions = evaluator._partition_dataset(X, list(X.columns), X.columns[0])
+    more_parts = len(partitions)
+
+    assert less_parts > 0
+    assert more_parts > 0
+    assert less_parts < more_parts
+
+
+def test_l_diversity_validation() -> None:
+    X, y = load_breast_cancer(return_X_y=True, as_frame=True)
+
+    evaluator = DatasetAnonymization(k_threshold=1, l_diversity=1)
+    assert evaluator.is_anonymous(X, sensitive_columns=[X.columns[0]]) is True
+
+    evaluator = DatasetAnonymization(k_threshold=2, l_diversity=1)
+    assert evaluator.is_anonymous(X, sensitive_columns=[X.columns[0]]) is False
+
+
+@pytest.mark.parametrize("k_threshold", [2, 5, 15, 30])
+@pytest.mark.parametrize("l_diversity", [1, 2])
+def test_l_diversity_anonymize_column(k_threshold: int, l_diversity: int) -> None:
+    X, y = load_diabetes(return_X_y=True, as_frame=True)
+    X["target"] = y
+
+    sensitive_features = ["target"]
+
+    evaluator = DatasetAnonymization(k_threshold=k_threshold, l_diversity=l_diversity)
+    assert evaluator.is_anonymous(X, sensitive_features) is False
+
+    anon_df = evaluator.anonymize_column(X, sensitive_column=sensitive_features[0])
+
+    assert evaluator.is_anonymous(anon_df, sensitive_features) is True
+    assert len(anon_df.drop_duplicates()) > 1
+
+
+@pytest.mark.parametrize("k_threshold", [2, 5, 15, 30])
+@pytest.mark.parametrize("l_diversity", [1, 2, 5])
+def test_l_diversity_anonymize_columns(k_threshold: int, l_diversity: int) -> None:
+    X, y = load_diabetes(return_X_y=True, as_frame=True)
+    X["target"] = y
+
+    sensitive_features = list(X.columns)[:2]
+
+    evaluator = DatasetAnonymization(k_threshold=k_threshold, l_diversity=l_diversity)
+    assert evaluator.is_anonymous(X, sensitive_features) is False
+
+    anon_df = evaluator.anonymize_columns(X, sensitive_columns=[sensitive_features[0]])
+
+    assert evaluator.is_anonymous(anon_df, sensitive_features) is True
+    assert len(anon_df.drop_duplicates()) > 1
+
+
+@pytest.mark.parametrize("k_threshold", [2, 5, 15, 30])
+@pytest.mark.parametrize("l_diversity", [1, 2, 5])
+def test_l_diversity_anonymize_full(k_threshold: int, l_diversity: int) -> None:
+    X, y = load_diabetes(return_X_y=True, as_frame=True)
+    X["target"] = y
+
+    sensitive_features = list(X.columns)[:2]
+
+    evaluator = DatasetAnonymization(k_threshold=k_threshold, l_diversity=l_diversity)
+    assert evaluator.is_anonymous(X, sensitive_features) is False
+
+    anon_df = evaluator.anonymize(X)
+
+    assert evaluator.is_anonymous(anon_df, sensitive_features) is True
+    assert len(anon_df.drop_duplicates()) > 1
+
+
+def test_t_closeness_sanity() -> None:
+    evaluator = DatasetAnonymization(
+        k_threshold=44,
+        l_diversity=33,
+        t_threshold=0.6,
+        categorical_limit=11,
+        max_partitions=22,
+    )
+
+    assert evaluator.categorical_limit == 11
+    assert evaluator.k_threshold == 44
+    assert evaluator.t_threshold == 0.6
+    assert evaluator.l_diversity == 33
+    assert evaluator.max_partitions == 22
+
+
+def test_t_closeness_get_spans() -> None:
+    X, y = load_diabetes(return_X_y=True, as_frame=True)
+    evaluator = DatasetAnonymization(t_threshold=0.5)
+
+    spans = evaluator._get_spans(X, X.index)
+
+    assert set(spans.keys()) == set(X.columns)
+    assert evaluator._get_categoricals(X) == ["sex"]
+
+
+def test_t_closeness_split() -> None:
+    X, y = load_diabetes(return_X_y=True, as_frame=True)
+    evaluator = DatasetAnonymization(t_threshold=0.5)
+
+    categoricals = evaluator._get_categoricals(X)
+    for col in X.columns:
+        lhs, rhs = evaluator._split(X, X.index, col, categoricals)
+
+        assert len(lhs) > 0
+        assert len(rhs) > 0
+        assert len(lhs) + len(rhs) == len(X)
+
+        assert evaluator._is_partition_anonymous(X, lhs, X.columns[0]) is True
+        assert evaluator._is_partition_anonymous(X, rhs, X.columns[0]) is True
+
+
+def test_t_closeness_partition() -> None:
+    X, y = load_diabetes(return_X_y=True, as_frame=True)
+    evaluator = DatasetAnonymization(t_threshold=0.1)
+    partitions = evaluator._partition_dataset(X, list(X.columns), "sex")
+    less_parts = len(partitions)
+
+    evaluator = DatasetAnonymization(t_threshold=0.9)
+    partitions = evaluator._partition_dataset(X, list(X.columns), "sex")
+    more_parts = len(partitions)
+
+    assert less_parts > 0
+    assert more_parts > 0
+    assert less_parts < more_parts
+
+
+def test_t_closeness_validation() -> None:
+    X, y = load_diabetes(return_X_y=True, as_frame=True)
+
+    evaluator = DatasetAnonymization(k_threshold=1, t_threshold=0.2)
+    assert evaluator.is_anonymous(X, sensitive_columns=["sex"]) is True
+
+    evaluator = DatasetAnonymization(k_threshold=2, t_threshold=0.5)
+    assert evaluator.is_anonymous(X, sensitive_columns=["sex"]) is False
+
+
+@pytest.mark.parametrize("k_threshold", [2, 5, 15, 30])
+@pytest.mark.parametrize("l_diversity", [1, 2])
+@pytest.mark.parametrize("t_threshold", [0.2, 0.5])
+def test_t_closeness_anonymize_column(
+    k_threshold: int, l_diversity: int, t_threshold: float
+) -> None:
+    X, y = load_diabetes(return_X_y=True, as_frame=True)
+    X["target"] = y
+
+    sensitive_features = ["target"]
+
+    evaluator = DatasetAnonymization(
+        k_threshold=k_threshold, l_diversity=l_diversity, t_threshold=t_threshold
+    )
+    assert evaluator.is_anonymous(X, sensitive_features) is False
+
+    anon_df = evaluator.anonymize_column(X, sensitive_column=sensitive_features[0])
+
+    assert evaluator.is_anonymous(anon_df, sensitive_features) is True
+    assert len(anon_df.drop_duplicates()) > 1
+
+
+@pytest.mark.parametrize("k_threshold", [2, 5, 15, 30])
+@pytest.mark.parametrize("l_diversity", [1, 2, 5])
+@pytest.mark.parametrize("t_threshold", [0.2, 0.5])
+def test_t_closeness_anonymize_columns(
+    k_threshold: int, l_diversity: int, t_threshold: float
+) -> None:
+    X, y = load_diabetes(return_X_y=True, as_frame=True)
+    X["target"] = y
+
+    sensitive_features = list(X.columns)[:2]
+
+    evaluator = DatasetAnonymization(
+        k_threshold=k_threshold, l_diversity=l_diversity, t_threshold=t_threshold
+    )
+    assert evaluator.is_anonymous(X, sensitive_features) is False
+
+    anon_df = evaluator.anonymize_column(X, sensitive_column=sensitive_features[0])
+
+    assert evaluator.is_anonymous(anon_df, sensitive_features) is True
+    assert len(anon_df.drop_duplicates()) > 1
+
+
+@pytest.mark.parametrize("k_threshold", [2, 5, 15, 30])
+@pytest.mark.parametrize("l_diversity", [1, 2, 5])
+@pytest.mark.parametrize("t_threshold", [0.2, 0.5])
+def test_t_closeness_anonymize_full(
+    k_threshold: int, l_diversity: int, t_threshold: float
+) -> None:
+    X, y = load_diabetes(return_X_y=True, as_frame=True)
+    X["target"] = y
+
+    sensitive_features = list(X.columns)[:2]
+
+    evaluator = DatasetAnonymization(
+        k_threshold=k_threshold, l_diversity=l_diversity, t_threshold=t_threshold
+    )
+    assert evaluator.is_anonymous(X, sensitive_features) is False
+
+    anon_df = evaluator.anonymize(X)
+
+    assert evaluator.is_anonymous(anon_df, sensitive_features) is True
+    assert len(anon_df.drop_duplicates()) > 1
