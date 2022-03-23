6ea6dbb1d38195097fbed57c68e514b242211b53
Bogdan Cebere <bogdan.cebere@gmail.com>
2022-03-23 12:07:38 +0000

    benchmark tests

--- tests/benchmarks/test_benchmarks.py
+++ tests/benchmarks/test_benchmarks.py
@@ -1,4 +1,5 @@
 # third party
+import pytest
 from sklearn.datasets import load_diabetes
 
 # synthcity absolute
@@ -20,3 +21,34 @@ def test_benchmark_sanity() -> None:
     )
 
     Benchmarks.print(scores)
+
+
+def test_benchmark_invalid_plugin() -> None:
+    X, y = load_diabetes(return_X_y=True, as_frame=True)
+
+    with pytest.raises(ValueError):
+        Benchmarks.evaluate(
+            [
+                "invalid",
+                "random_noise",
+            ],
+            X,
+            y,
+            sensitive_columns=["sex"],
+            metrics={"sanity": ["common_rows_proportion", "data_mismatch_score"]},
+        )
+
+
+def test_benchmark_invalid_metric() -> None:
+    X, y = load_diabetes(return_X_y=True, as_frame=True)
+
+    score = Benchmarks.evaluate(
+        [
+            "random_noise",
+        ],
+        X,
+        y,
+        sensitive_columns=["sex"],
+        metrics={"sanity": ["invalid"]},
+    )
+    assert len(score["random_noise"]) == 0
