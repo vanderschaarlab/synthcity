899a491817da5926c9e5ebd21541a00b1b8dca2a
Bogdan Cebere <bogdan.cebere@gmail.com>
2022-03-25 16:59:08 +0000

    add TVAE

---
+++ src/synthcity/plugins/plugin_tvae.py
@@ -0,0 +1,69 @@
+# stdlib
+from typing import Any, List
+
+# third party
+import pandas as pd
+from sdv.tabular import TVAE
+
+# synthcity absolute
+from synthcity.plugins.core.distribution import Distribution
+from synthcity.plugins.core.plugin import Plugin
+from synthcity.plugins.core.schema import Schema
+
+
+class TVAEPlugin(Plugin):
+    """TVAE plugin.
+
+    Example:
+        >>> from synthcity.plugins import Plugins
+        >>> plugin = Plugins().get("tvae")
+        >>> from sklearn.datasets import load_iris
+        >>> X = load_iris()
+        >>> plugin.fit(X)
+        >>> plugin.generate()
+    """
+
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
+
+        self.model = TVAE()
+
+    @staticmethod
+    def name() -> str:
+        return "tvae"
+
+    @staticmethod
+    def type() -> str:
+        return "vae"
+
+    @staticmethod
+    def hyperparameter_space(**kwargs: Any) -> List[Distribution]:
+        return []
+
+    def _fit(self, X: pd.DataFrame, *args: Any, **kwargs: Any) -> "TVAEPlugin":
+        self.model.fit(X)
+        return self
+
+    def _generate(self, count: int, syn_schema: Schema, **kwargs: Any) -> pd.DataFrame:
+        constraints = syn_schema.as_constraints()
+
+        data_synth = pd.DataFrame([], columns=self.schema().features())
+        for it in range(self.sampling_patience):
+            iter_samples = self.model.sample(count)
+            iter_samples_df = pd.DataFrame(
+                iter_samples, columns=self.schema().features()
+            )
+            iter_samples_df = syn_schema.adapt_dtypes(iter_samples_df)
+
+            iter_synth_valid = constraints.match(iter_samples_df)
+            data_synth = pd.concat([data_synth, iter_synth_valid], ignore_index=True)
+
+            if len(data_synth) >= count:
+                break
+
+        data_synth = syn_schema.adapt_dtypes(data_synth).head(count)
+
+        return data_synth
+
+
+plugin = TVAEPlugin
