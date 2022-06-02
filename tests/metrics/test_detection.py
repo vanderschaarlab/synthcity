0a051dc174cca3af8d9dd1fef22d62be1fe22653
Bogdan Cebere <bogdan.cebere@gmail.com>
2022-06-02 12:33:48 +0100

    add time series to metrics

--- tests/metrics/test_detection.py
+++ tests/metrics/test_detection.py
@@ -14,7 +14,8 @@ from synthcity.metrics.eval_detection import (
     SyntheticDetectionXGB,
 )
 from synthcity.plugins import Plugin, Plugins
-from synthcity.plugins.core.dataloader import GenericDataLoader
+from synthcity.plugins.core.dataloader import GenericDataLoader, TimeSeriesDataLoader
+from synthcity.utils.datasets.time_series.google_stocks import GoogleStocksDataloader
 
 
 @pytest.mark.parametrize("reduction", ["mean", "max", "min"])
@@ -52,7 +53,7 @@ def test_detect_reduction(reduction: str, evaluator_t: Type) -> None:
         SyntheticDetectionMLP,
     ],
 )
-def test_detect_synth(test_plugin: Plugin, evaluator_t: Type) -> None:
+def test_detect_synth_generic(test_plugin: Plugin, evaluator_t: Type) -> None:
     X, y = load_iris(return_X_y=True, as_frame=True)
     X["target"] = y
     Xloader = GenericDataLoader(X)
@@ -75,6 +76,53 @@ def test_detect_synth(test_plugin: Plugin, evaluator_t: Type) -> None:
     score = evaluator.evaluate(
         Xloader,
         GenericDataLoader(X_rnd),
+    )["mean"]
+
+    assert score > 0
+    assert score <= 1
+    assert good_score < score
+
+    assert evaluator.type() == "detection"
+    assert evaluator.direction() == "minimize"
+
+
+@pytest.mark.parametrize("test_plugin", [Plugins().get("marginal_distributions")])
+@pytest.mark.parametrize(
+    "evaluator_t",
+    [
+        SyntheticDetectionXGB,
+    ],
+)
+def test_detect_synth_timeseries(test_plugin: Plugin, evaluator_t: Type) -> None:
+    static_data, temporal_data, outcome = GoogleStocksDataloader().load()
+    data = TimeSeriesDataLoader(
+        temporal_data=temporal_data,
+        static_data=static_data,
+        outcome=outcome,
+    )
+
+    test_plugin.fit(data)
+    data_gen = test_plugin.generate(200)
+
+    evaluator = evaluator_t()
+
+    good_score = evaluator.evaluate(
+        data,
+        data_gen,
+    )["mean"]
+
+    assert good_score > 0
+    assert good_score <= 1
+
+    sz = 200
+    data_rnd = TimeSeriesDataLoader.from_info(
+        pd.DataFrame(np.random.randn(sz, len(data.columns)), columns=data.columns),
+        data.info(),
+    )
+
+    score = evaluator.evaluate(
+        data,
+        data_rnd,
     )["mean"]
 
     assert score > 0
