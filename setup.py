3a09c6696a17c0f7732b5aebe8b78ca5db0607e1
Bogdan Cebere <bogdan.cebere@gmail.com>
2022-06-07 16:03:08 +0100

    update versioning

--- setup.py
+++ setup.py
@@ -1,17 +1,34 @@
-"""
-    Setup file for synthcity.
-    Use setup.cfg to configure your project.
+# stdlib
+import os
+import re
 
-    This file was generated with PyScaffold 4.1.1.
-    PyScaffold helps you to put up the scaffold of your new Python project.
-    Learn more under: https://pyscaffold.org/
-"""
 # third party
 from setuptools import setup
 
+PKG_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def read(fname: str) -> str:
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+def find_version() -> str:
+    version_file = read("src/synthcity/version.py")
+    version_re = r"__version__ = \"(?P<version>.+)\""
+    version_raw = re.match(version_re, version_file)
+
+    if version_raw is None:
+        return "0.0.1"
+
+    version = version_raw.group("version")
+    return version
+
+
 if __name__ == "__main__":
     try:
-        setup(use_scm_version={"version_scheme": "no-guess-dev"})
+        setup(
+            version=find_version(),
+        )
     except:  # noqa
         print(
             "\n\nAn error occurred while building the project, "
