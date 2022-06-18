1a890b66d5c4afa9d9c3b5e9e79509a7d16749e8
Bogdan Cebere <bogdan.cebere@gmail.com>
2022-06-18 22:05:23 +0100

    PAR plugin

---
+++ src/synthcity/plugins/time_series/plugin_probabilistic_ar.py
@@ -0,0 +1,106 @@
+"""
+Probabilistic autoregressive model
+"""
+# stdlib
+from typing import Any, List, Tuple
+
+# third party
+import pandas as pd
+from deepecho import PARModel
+
+# synthcity absolute
+from synthcity.plugins.core.dataloader import DataLoader, TimeSeriesDataLoader
+from synthcity.plugins.core.distribution import Distribution, IntegerDistribution
+from synthcity.plugins.core.plugin import Plugin
+from synthcity.plugins.core.schema import Schema
+from synthcity.utils.constants import DEVICE
+
+
+class ProbabilisticAutoregressivePlugin(Plugin):
+    """Synthetic time series generation using Probabilistic Autoregressive models.
+
+    Args:
+        n_iter: int
+            Maximum number of iterations in the Generator.
+
+    Example:
+        >>> from synthcity.plugins import Plugins
+        >>> from synthcity.utils.datasets.time_series.google_stocks import GoogleStocksDataloader
+        >>> from synthcity.plugins.core.dataloader import TimeSeriesDataLoader
+        >>>
+        >>> plugin = Plugins().get("probabilistic_ar")
+        >>> static, temporal, outcome = GoogleStocksDataloader(as_numpy=True).load()
+        >>> loader = TimeSeriesDataLoader(
+        >>>             temporal_data=temporal_data,
+        >>>             static_data=static_data,
+        >>>             outcome=outcome,
+        >>> )
+        >>> plugin.fit(loader)
+        >>> plugin.generate()
+    """
+
+    def __init__(
+        self,
+        n_iter: int = 1000,
+        sample_size: int = 1,
+        device: Any = DEVICE,
+        **kwargs: Any
+    ) -> None:
+        super().__init__()
+
+        self.model = PARModel(epochs=n_iter, sample_size=sample_size, verbose=False)
+
+    @staticmethod
+    def name() -> str:
+        return "probabilistic_ar"
+
+    @staticmethod
+    def type() -> str:
+        return "time_series"
+
+    @staticmethod
+    def hyperparameter_space(**kwargs: Any) -> List[Distribution]:
+        return [
+            IntegerDistribution(name="n_iter", low=100, high=1000, step=100),
+            IntegerDistribution(name="sample_size", low=1, high=10),
+        ]
+
+    def _fit(
+        self, X: DataLoader, *args: Any, **kwargs: Any
+    ) -> "ProbabilisticAutoregressivePlugin":
+        assert X.type() == "time_series"
+
+        seq_df, info = X.sequential_view()
+        self.info = info
+
+        id_col = info["id_feature"]
+        time_col = info["time_feature"]
+        static_cols = info["static_features"]
+        out_cols = info["outcome_features"]
+
+        # Train the static and temporal generator
+        self.model.fit(
+            data=seq_df,
+            entity_columns=[id_col],
+            context_columns=static_cols + out_cols,
+            sequence_index=time_col,
+        )
+
+        return self
+
+    def _generate(self, count: int, syn_schema: Schema, **kwargs: Any) -> pd.DataFrame:
+        def _sample(count: int) -> Tuple:
+            # Static and Temporal generation
+            data = self.model.sample(num_entities=count)
+            time_col = self.info["time_feature"]
+
+            # Decoding
+            data[time_col] = list(range(len(data)))
+            loader = TimeSeriesDataLoader.from_sequential_view(data, self.info)
+
+            return loader.unpack()
+
+        return self._safe_generate_time_series(_sample, count, syn_schema)
+
+
+plugin = ProbabilisticAutoregressivePlugin
---
+++ tests/plugins/time_series/test_probabilistic_ar.py
@@ -0,0 +1,68 @@
+# third party
+import pytest
+from ts_helpers import generate_fixtures
+
+# synthcity absolute
+from synthcity.plugins import Plugin
+from synthcity.plugins.core.dataloader import TimeSeriesDataLoader
+from synthcity.plugins.time_series.plugin_probabilistic_ar import plugin
+from synthcity.utils.datasets.time_series.google_stocks import GoogleStocksDataloader
+
+static_data, temporal_data, outcome = GoogleStocksDataloader().load()
+data = TimeSeriesDataLoader(
+    temporal_data=temporal_data,
+    static_data=static_data,
+    outcome=outcome,
+)
+plugin_name = "probabilistic_ar"
+
+
+@pytest.mark.parametrize("test_plugin", generate_fixtures(plugin_name, plugin))
+def test_plugin_sanity(test_plugin: Plugin) -> None:
+    assert test_plugin is not None
+
+
+@pytest.mark.parametrize("test_plugin", generate_fixtures(plugin_name, plugin))
+def test_plugin_name(test_plugin: Plugin) -> None:
+    assert test_plugin.name() == plugin_name
+
+
+@pytest.mark.parametrize("test_plugin", generate_fixtures(plugin_name, plugin))
+def test_plugin_type(test_plugin: Plugin) -> None:
+    assert test_plugin.type() == "time_series"
+
+
+@pytest.mark.parametrize("test_plugin", generate_fixtures(plugin_name, plugin))
+def test_plugin_hyperparams(test_plugin: Plugin) -> None:
+    assert len(test_plugin.hyperparameter_space()) == 2
+
+
+def test_plugin_fit() -> None:
+    test_plugin = plugin(
+        n_iter=10,
+    )
+
+    test_plugin.fit(data)
+
+
+def test_plugin_generate() -> None:
+    test_plugin = plugin(
+        n_iter=10,
+    )
+    test_plugin.fit(data)
+
+    X_gen = test_plugin.generate()
+    assert len(X_gen) == len(data)
+    assert test_plugin.schema_includes(X_gen)
+
+    X_gen = test_plugin.generate(50)
+    assert len(X_gen) == 50
+    assert test_plugin.schema_includes(X_gen)
+    assert list(X_gen.columns) == list(data.columns)
+
+
+def test_sample_hyperparams() -> None:
+    for i in range(100):
+        args = plugin.sample_hyperparameters()
+
+        assert plugin(**args) is not None
