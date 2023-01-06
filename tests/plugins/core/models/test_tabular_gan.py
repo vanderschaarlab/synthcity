da2f7dd4b7571ca2518c22110a74019e9bcc19ae
Bogdan Cebere <bogdan.cebere@gmail.com>
2023-01-06 13:21:18 +0200

    cleanup tests

--- tests/plugins/core/models/test_tabular_gan.py
+++ tests/plugins/core/models/test_tabular_gan.py
@@ -174,8 +174,7 @@ def test_gan_generation_with_early_stopping(patience_metric: Tuple[str, str]) ->
     assert generated.shape == (10, X.shape[1])
 
 
-@pytest.mark.skipif(sys.platform != "linux", reason="Linux only for faster results")
-@pytest.mark.skipif(sys.version_info < (3, 9), reason="requires python3.9 or higher")
+@pytest.mark.slow
 def test_gan_sampling_adjustment() -> None:
     X = get_airfoil_dataset()
 
