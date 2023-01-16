223c745eddf70895f0526764efcc2b1908ae4ee6
Bogdan Cebere <bogdan.cebere@gmail.com>
2023-01-16 09:08:09 +0200

    update docs

--- src/synthcity/plugins/core/plugin.py
+++ src/synthcity/plugins/core/plugin.py
@@ -165,7 +165,42 @@ class Plugin(Serializable, metaclass=ABCMeta):
             X: DataLoader.
                 The reference dataset.
             cond: Optional, Union[pd.DataFrame, pd.Series, np.ndarray]
-                Training Conditional
+                Optional Training Conditional.
+                The training conditional can be used to control to output of some models, like GANs or VAEs. The content can be anything, as long as it maps to the training dataset X.
+                Usage example:
+                    >>> from sklearn.datasets import load_iris
+                    >>> from synthcity.plugins.core.dataloader import GenericDataLoader
+                    >>> from synthcity.plugins.core.constraints import Constraints
+                    >>>
+                    >>> # Load in `test_plugin` the generative model of choice
+                    >>> # ....
+                    >>>
+                    >>> X, y = load_iris(as_frame=True, return_X_y=True)
+                    >>> X["target"] = y
+                    >>>
+                    >>> X = GenericDataLoader(X)
+                    >>> test_plugin.fit(X, cond=y)
+                    >>>
+                    >>> count = 10
+                    >>> X_gen = test_plugin.generate(count, cond=np.ones(count))
+                    >>>
+                    >>> # The Conditional only optimizes the output generation
+                    >>> # for GANs and VAEs, but does NOT guarantee the samples
+                    >>> # are only from that condition.
+                    >>> # If you want to guarantee that output contains only
+                    >>> # "target" == 1 samples, use Constraints.
+                    >>>
+                    >>> constraints = Constraints(
+                    >>>     rules=[
+                    >>>         ("target", "==", 1),
+                    >>>     ]
+                    >>> )
+                    >>> X_gen = test_plugin.generate(count,
+                    >>>         cond=np.ones(count),
+                    >>>         constraints=constraints
+                    >>>        )
+                    >>> assert (X_gen["target"] == 1).all()
+
         Returns:
             self
         """
@@ -233,10 +268,25 @@ class Plugin(Serializable, metaclass=ABCMeta):
             count: optional int.
                 The number of samples to generate. If None, it generated len(reference_dataset) samples.
             constraints: optional Constraints
-                Optional constraints to apply on the generated data. If none, the reference schema constraints are applied.
-            cond: Optional, Union[pd.DataFrame, pd.Series, np.ndarray]
-                Generation Conditional
+                Optional constraints to apply on the generated data. If none, the reference schema constraints are applied. The constraints are model agnostic, and will filter the output of the generative model.
+                Usage example:
+                    >>> from synthcity.plugins.core.constraints import Constraints
+                    >>> constraints = Constraints(
+                    >>>   rules=[
+                    >>>             ("InterestingFeature", "==", 0),
+                    >>>         ]
+                    >>>     )
+                    >>>
+                    >>> syn_data = syn_model.generate(
+                            count=count,
+                            constraints=constraints
+                        ).dataframe()
+                    >>>
+                    >>> assert (syn_data["InterestingFeature"] == 0).all()
 
+            cond: Optional, Union[pd.DataFrame, pd.Series, np.ndarray]
+                Optional Generation Conditional. The conditional can be used only if the model was trained using a conditional too.
+                If provided, it must have `count` length.
         Returns:
             <count> synthetic samples
         """
