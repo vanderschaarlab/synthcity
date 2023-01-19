e36bb5a5092abab0fc40ff5717423e7911b20de8
Bogdan Cebere <bogdan.cebere@gmail.com>
2023-01-19 07:38:54 +0000

    optuna fixes

--- src/synthcity/utils/redis_wrapper.py
+++ src/synthcity/utils/redis_wrapper.py
@@ -2,8 +2,8 @@
 import os
 
 # third party
-import optuna
 import redis as rediscli
+from optuna.storages import JournalRedisStorage, JournalStorage
 
 REDIS_HOST = os.getenv("REDIS_HOST", "127.0.0.1")
 
@@ -17,14 +17,11 @@ class RedisBackend:
     ):
         self.url = f"redis://{host}:{port}/"
 
-        self._optuna_storage = optuna.storages.RedisStorage(url=self.url)
+        self._optuna_storage = JournalStorage(JournalRedisStorage(url=self.url))
         self._client = rediscli.Redis.from_url(self.url)
 
-    def optuna(self) -> optuna.storages.RedisStorage:
+    def optuna(self) -> JournalStorage:
         return self._optuna_storage
 
     def client(self) -> rediscli.Redis:
         return self._client
-
-
-backend = RedisBackend()
