52be80f2ecb3aab0c9a3a5cd782744814127872d
TZCai <13818704679@163.com>
2023-03-16 15:34:47 +0100

    update EarlyStopping

--- src/synthcity/utils/callbacks.py
+++ src/synthcity/utils/callbacks.py
@@ -1,6 +1,5 @@
 # stdlib
 from abc import ABC, abstractmethod
-from typing import Optional
 
 # third party
 import numpy as np
@@ -34,25 +33,29 @@ class Callback(ABC):
 class EarlyStopping(Callback):
     def __init__(
         self,
+        patience_metric: WeightedMetrics,
         patience: int = 5,
         min_epochs: int = 100,
-        patience_metric: Optional[WeightedMetrics] = None,
     ) -> None:
         self.patience = patience
-        self.patience_metric = patience_metric
         self.min_epochs = min_epochs
+        self.patience_metric = patience_metric
         self.best_score = self._init_patience_score()
         self.best_model_state = None
         self.wait = 0
         self._epochs = 0
 
+    def on_fit_begin(self, model: nn.Module) -> None:
+        self.X_val = model.X_val
+        if isinstance(self.X_val, Tensor):
+            self.X_val = self.X_val.detach().cpu().numpy()
+
+    def on_epoch_begin(self, model: nn.Module) -> None:
+        pass
+
     def on_epoch_end(self, model: nn.Module) -> None:
         self._epochs += 1
         if self.patience_metric is not None:
-            if not hasattr(self, "X_val"):
-                self.X_val = model.X_val
-                if isinstance(self.X_val, Tensor):
-                    self.X_val = self.X_val.detach().cpu().numpy()
             self._evaluate_patience_metric(model)
         if self.wait >= self.patience and self._epochs >= self.min_epochs:
             raise StopIteration("Early stopping")
@@ -62,9 +65,7 @@ class EarlyStopping(Callback):
             model.load_state_dict(self.best_model_state)  # type: ignore
 
     def _init_patience_score(self) -> float:
-        if self.patience_metric is None:
-            return 0
-        elif self.patience_metric.direction() == "minimize":
+        if self.patience_metric.direction() == "minimize":
             return np.inf
         else:
             return -np.inf
@@ -73,12 +74,12 @@ class EarlyStopping(Callback):
         X_val = self.X_val
         X_syn = model.generate(len(X_val))
 
-        new_score = self.patience_metric.evaluate(  # type: ignore
+        new_score = self.patience_metric.evaluate(
             pd.DataFrame(X_val),
             pd.DataFrame(X_syn),
         )
 
-        if self.patience_metric.direction() == "minimize":  # type: ignore
+        if self.patience_metric.direction() == "minimize":
             is_new_best = new_score < self.best_score
         else:
             is_new_best = new_score > self.best_score
