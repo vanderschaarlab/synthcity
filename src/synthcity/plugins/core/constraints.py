27553e958c5d2e4a73f042628cc50047a374ea3e
TZCai <13818704679@163.com>
2023-04-10 13:15:34 +0200

    update

--- src/synthcity/plugins/core/constraints.py
+++ src/synthcity/plugins/core/constraints.py
@@ -164,7 +164,7 @@ class Constraints(BaseModel):
                 thresh,
             )
             if res.sum() < prev:
-                log.critical(
+                log.info(
                     f"[{feature}] quality loss for constraints {op} = {thresh}. Remaining {res.sum()}. prev length {prev}. Original dtype {X[feature].dtype}.",
                 )
         return res
