2e65e014b7cc06bd891b07884fb80a8a23524e61
Rob Davis <robsdavis473@gmail.com>
2024-09-03 16:10:39 +0100

    add retries to get_airfoil

--- tests/plugins/core/models/helpers.py
+++ tests/plugins/core/models/helpers.py
@@ -1,8 +1,24 @@
+# stdlib
+import urllib.error
+
 # third party
 import pandas as pd
+from tenacity import retry, retry_if_exception_type, stop_after_attempt, wait_fixed
 
 
+@retry(
+    stop=stop_after_attempt(5),  # Retry up to 5 times
+    wait=wait_fixed(2),  # Wait 2 seconds between retries
+    retry=retry_if_exception_type(urllib.error.HTTPError),  # Retry on HTTPError
+)
 def get_airfoil_dataset() -> pd.DataFrame:
+    """
+    Downloads the Airfoil Self-Noise dataset and returns it as a DataFrame.
+
+    Returns:
+        pd.DataFrame: The Airfoil Self-Noise dataset.
+    """
+    # Read the dataset from the URL
     df = pd.read_csv(
         "https://archive.ics.uci.edu/static/public/291/airfoil+self+noise.zip",
         sep="\t",
--- tests/plugins/privacy/fhelpers.py
+++ tests/plugins/privacy/fhelpers.py
@@ -1,8 +1,10 @@
 # stdlib
+import urllib.error
 from typing import Dict, List, Type
 
 # third party
 import pandas as pd
+from tenacity import retry, retry_if_exception_type, stop_after_attempt, wait_fixed
 
 # synthcity absolute
 from synthcity.plugins import Plugin, Plugins
@@ -25,7 +27,19 @@ def generate_fixtures(name: str, plugin: Type, plugin_args: Dict = {}) -> List:
     return [from_api(), from_module(), from_serde()]
 
 
+@retry(
+    stop=stop_after_attempt(5),  # Retry up to 5 times
+    wait=wait_fixed(2),  # Wait 2 seconds between retries
+    retry=retry_if_exception_type(urllib.error.HTTPError),  # Retry on HTTPError
+)
 def get_airfoil_dataset() -> pd.DataFrame:
+    """
+    Downloads the Airfoil Self-Noise dataset and returns it as a DataFrame.
+
+    Returns:
+        pd.DataFrame: The Airfoil Self-Noise dataset.
+    """
+    # Read the dataset from the URL
     df = pd.read_csv(
         "https://archive.ics.uci.edu/static/public/291/airfoil+self+noise.zip",
         sep="\t",
