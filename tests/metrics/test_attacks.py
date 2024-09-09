05430cecc779f0b3ef1ff50e8eba652f3480c4ca
Rob Davis <robsdavis473@gmail.com>
2024-09-09 17:17:47 +0100

    update workflows

--- tests/metrics/test_attacks.py
+++ tests/metrics/test_attacks.py
@@ -1,5 +1,4 @@
 # stdlib
-import sys
 from typing import Type
 
 # third party
@@ -15,21 +14,16 @@ from synthcity.metrics.eval_attacks import (
 from synthcity.plugins import Plugins
 from synthcity.plugins.core.dataloader import GenericDataLoader
 
-# Define parameters for different OSes
-if sys.platform == "darwin":  # macOS
-    reduction_params = ["mean"]  # Only run "mean" on macOS
-    evaluator_params = [DataLeakageLinear]  # Only run this evaluator on macOS
-else:  # Linux and Windows
-    reduction_params = ["mean", "max", "min"]  # Run all on Linux/Windows
-    evaluator_params = [
+
+@pytest.mark.parametrize("reduction", ["mean", "max", "min"])
+@pytest.mark.parametrize(
+    "evaluator_t",
+    [
         DataLeakageLinear,
         DataLeakageXGB,
         DataLeakageMLP,
-    ]  # All evaluators
-
-
-@pytest.mark.parametrize("reduction", reduction_params)
-@pytest.mark.parametrize("evaluator_t", evaluator_params)
+    ],
+)
 def test_reduction(reduction: str, evaluator_t: Type) -> None:
     X, y = load_diabetes(return_X_y=True, as_frame=True)
     X["target"] = y
@@ -58,7 +52,14 @@ def test_reduction(reduction: str, evaluator_t: Type) -> None:
     assert def_score == score[reduction]
 
 
-@pytest.mark.parametrize("evaluator_t", evaluator_params)
+@pytest.mark.parametrize(
+    "evaluator_t",
+    [
+        DataLeakageLinear,
+        DataLeakageXGB,
+        DataLeakageMLP,
+    ],
+)
 def test_evaluate_sensitive_data_leakage(evaluator_t: Type) -> None:
     X, y = load_diabetes(return_X_y=True, as_frame=True)
     X["target"] = y
